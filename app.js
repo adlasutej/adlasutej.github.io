84b65993e6170ec3cc64755ba0b68633481cde15
Sutej <adla.sutej@wittybrains.com>
2018-04-03 17:54:42 +0530

    implemented optional functionality of 1st assignment

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 	function myController($scope){
 		$scope.sampleInput="";
 		$scope.message="";
+		$scope.backgroundClass={};
+		$scope.fontColor="";
 		$scope.check = function(){
 			var lst = $scope.sampleInput.split(/[, ]+/);
 			var length = 0;
@@ -13,12 +15,21 @@
 				if(lst[i].length > 0)
 					length+=1;
 			}
-			if(length === 0)
+			if(length === 0){
 				$scope.message = "Please enter data first";
-			else if (length <= 3)
+				$scope.backgroundClass = {'border-color':'green'};
+				$scope.fontColor = {'color':'green'};
+			}
+			else if (length <= 3){
 				$scope.message = "Enjoy!";
-			else
+				$scope.backgroundClass = {'border-color':'green'};
+				$scope.fontColor = {'color':'green'};
+			}
+			else{
 				$scope.message = "Too much!";
+				$scope.backgroundClass = {'border-color':'red'};
+				$scope.fontColor = {'color':'red'};
+			}
 		};
 	}
 })();
